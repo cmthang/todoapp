d984df481848b22718cdc6265f9f3823baad4234
thang <cmthang2407@gmail.com>
2022-08-17 17:34:37 +0700

    fix button copy does not work on http

--- app.js
+++ app.js
@@ -100,8 +100,35 @@ Vue.createApp ({
             return this.task = password;
         },
 
+        //does not work on http 
+        // copy(task){
+        //     navigator.clipboard.writeText(task.name);
+        // }
+
         copy(task){
-            navigator.clipboard.writeText(task.name);
+            // navigator clipboard api needs a secure context (https)
+            if (navigator.clipboard && window.isSecureContext) {
+                // navigator clipboard api method'
+                return navigator.clipboard.writeText(task.name);
+            } 
+            else 
+            {
+                // text area method
+                let textArea = document.createElement("textarea");
+                textArea.value = textToCopy;
+                // make the textarea out of viewport
+                textArea.style.position = "fixed";
+                textArea.style.left = "-999999px";
+                textArea.style.top = "-999999px";
+                document.body.appendChild(textArea);
+                textArea.focus();
+                textArea.select();
+                return new Promise((res, rej) => {
+                    // here the magic happens
+                    document.execCommand('copy') ? res() : rej();
+                    textArea.remove();
+                });
+            }        
         }
 
     },
